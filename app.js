d4a558caaf62619b3a0afdb73760615010163504
sanjay <167325905+Sanjay181912@users.noreply.github.com>
2026-03-26 17:05:40 +0530

    added home routes

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use((req, res, next) => {
 app.get("/health",(req,res)=>{
   res.send("Health is okay")
 })
-app.use("/", userRouter);
+app.use("/", listingsRouter);
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 
